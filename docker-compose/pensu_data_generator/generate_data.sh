f045f2a2a660cc58dbccc66329151f71a6aeb070
iyuvalk <iyuvalk@gmail.com>
2020-04-24 08:44:43 +0300

    Switched to using docker stack deploy instead of docker-compose up although it should still work that way too. (#2)

--- docker-compose/pensu_data_generator/generate_data.sh
+++ docker-compose/pensu_data_generator/generate_data.sh
@@ -8,11 +8,11 @@ test_data_total_lines=$(cat $DATA_FILE | wc -l)
 cur_line=$START_AT_LINE
 
 while true; do
-  echo my_stats.pensu_test_metrics.load.5min_avg `uptime | awk -F, '{print $6}' | awk '{print $1}'` `date +%s` | ./kafka_2.13-2.4.0/bin/kafka-console-producer.sh --broker-list pensu_kafka1:9092 --topic metrics
-  echo my_stats.pensu_test_metrics.memory.used `free | grep '^Mem' | awk '{print $3}'` `date +%s` | ./kafka_2.13-2.4.0/bin/kafka-console-producer.sh --broker-list pensu_kafka1:9092 --topic metrics
-  echo my_stats.pensu_test_metrics.network.rx_bytes.eth0 `ifconfig eth0 | grep 'RX packets' | awk '{print $5}'` `date +%s` | ./kafka_2.13-2.4.0/bin/kafka-console-producer.sh --broker-list pensu_kafka1:9092 --topic metrics
-  echo my_stats.pensu_test_metrics.network.tx_bytes.eth0 `ifconfig eth0 | grep 'TX packets' | awk '{print $5}'` `date +%s` | ./kafka_2.13-2.4.0/bin/kafka-console-producer.sh --broker-list pensu_kafka1:9092 --topic metrics
-  echo my_stats.pensu_test_metrics.test_data `sed "${cur_line}q;d" $DATA_FILE | sed 's/\r//g' | cut -d, -f${DATA_COLUMN}` `date +%s` | ./kafka_2.13-2.4.0/bin/kafka-console-producer.sh --broker-list pensu_kafka1:9092 --topic metrics
+  echo my_stats.pensu_test_metrics.load.5min_avg `uptime | awk -F, '{print $6}' | awk '{print $1}'` `date +%s` | ./kafka_2.13-2.4.0/bin/kafka-console-producer.sh --broker-list pensu_kafka:9092 --topic metrics
+  echo my_stats.pensu_test_metrics.memory.used `free | grep '^Mem' | awk '{print $3}'` `date +%s` | ./kafka_2.13-2.4.0/bin/kafka-console-producer.sh --broker-list pensu_kafka:9092 --topic metrics
+  echo my_stats.pensu_test_metrics.network.rx_bytes.eth0 `ifconfig eth0 | grep 'RX packets' | awk '{print $5}'` `date +%s` | ./kafka_2.13-2.4.0/bin/kafka-console-producer.sh --broker-list pensu_kafka:9092 --topic metrics
+  echo my_stats.pensu_test_metrics.network.tx_bytes.eth0 `ifconfig eth0 | grep 'TX packets' | awk '{print $5}'` `date +%s` | ./kafka_2.13-2.4.0/bin/kafka-console-producer.sh --broker-list pensu_kafka:9092 --topic metrics
+  echo my_stats.pensu_test_metrics.test_data `sed "${cur_line}q;d" $DATA_FILE | sed 's/\r//g' | cut -d, -f${DATA_COLUMN}` `date +%s` | ./kafka_2.13-2.4.0/bin/kafka-console-producer.sh --broker-list pensu_kafka:9092 --topic metrics
 
   sleep 1
   (( cur_line++ ))
